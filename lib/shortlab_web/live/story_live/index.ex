9b9cf33c9a47cb5a63f529f2dc3bf46531bf7147
digzom <dicksonmelo@gmail.com>
2023-10-07 08:01:15 -0300

    wip: trying to handle events

--- lib/shortlab_web/live/story_live/index.ex
+++ lib/shortlab_web/live/story_live/index.ex
@@ -1,7 +1,6 @@
 defmodule ShortlabWeb.StoryLive.Index do
+  require Logger
   use ShortlabWeb, :live_view
-
-  alias Shortlab.Stories
 
   @impl true
   def mount(_params, _session, socket) do
@@ -29,14 +28,90 @@ defmodule ShortlabWeb.StoryLive.Index do
     {:noreply, apply_action(socket, socket.assigns.live_action, params)}
   end
 
-  defp apply_action(socket, :create_mr, _params) do
+  @impl true
+  def handle_event("save_params", params, socket) do
+    params
+    |> Map.filter(fn {_k, v} ->
+      v == "true"
+    end)
+    |> Map.keys()
+
+    {:noreply, socket |> push_navigate(to: ~p"~/stories")}
+  end
+
+  def handle_event("create_mr", params, socket) do
+    IO.inspect(params)
+
+    # with {:ok, %Tesla.Env{body: %{"name" => branch_name}}} <- create_branch(),
+    #      {:ok, %Tesla.Env{body: _mr_body}} <- create_mr(branch_name) do
+    #   socket
+    #   |> assign(:page_title, "fodase")
+    #   |> assign(:story, nil)
+    #   |> put_flash(:success, "deu certo man")
+    #   |> push_patch(to: ~p"/stories")
+    # else
+    #   e ->
+    #     Logger.error("#{inspect(e)}")
+    # end
+    #
     socket
+    |> assign(:page_title, "fodase")
+    |> assign(:story, nil)
+    |> put_flash(:success, "deu certo man")
+    |> push_patch(to: ~p"/stories")
   end
 
   defp apply_action(socket, :index, _params) do
     socket
     |> assign(:page_title, "Listing Stories")
+    |> assign(:form, %{"mrs" => []})
     |> assign(:story, nil)
+  end
+
+  defp create_mr(branch_name) do
+    middleware = [
+      {Tesla.Middleware.BaseUrl, "https://gitlab.com/api/v4/projects/46562739/repository"},
+      Tesla.Middleware.JSON,
+      {Tesla.Middleware.Headers, [{"PRIVATE-TOKEN", "glpat-aHjS9FG5iQHYAa2vqx5u"}]}
+    ]
+
+    client = Tesla.client(middleware)
+
+    Tesla.post(client, "https://gitlab.com/api/v4/projects/46562739/merge_requests", %{
+      "allow_maintainer_to_push" => false,
+      "description" =>
+        "thats the description of this MR doing #{Enum.random(0..100_000_000_000_000_000)}",
+      "id" => 46_562_739,
+      "remove_source_branch" => true,
+      "source_branch" => branch_name,
+      "target_branch" => "production",
+      "title" => "Teste pela API numero #{Enum.random(0..100_000_000_000_000_000)}"
+    })
+  end
+
+  defp create_branch do
+    middleware = [
+      {Tesla.Middleware.BaseUrl, "https://gitlab.com/api/v4/projects/46562739/repository"},
+      Tesla.Middleware.JSON,
+      {Tesla.Middleware.Headers, [{"PRIVATE-TOKEN", "glpat-aHjS9FG5iQHYAa2vqx5u"}]}
+    ]
+
+    client = Tesla.client(middleware)
+
+    timestamp =
+      DateTime.utc_now()
+      |> DateTime.to_string()
+      |> String.replace("-", "")
+      |> String.replace(" ", "")
+      |> String.replace(":", "")
+      |> String.replace(".", "")
+      |> String.replace("Z", "")
+
+    Tesla.post(client, "https://gitlab.com/api/v4/projects/46562739/repository/branches", %{
+      "id" => 46_562_739,
+      "branch" => "release-with-elixir-#{timestamp}",
+      "ref" => "master"
+    })
   end
 
   # Saving these two just to see how to use stream_insert
